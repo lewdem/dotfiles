658674c83b110dcd144e70b50b252e8f4838aaa0
zultrn <arslong00vitabrevis@gmail.com>
2024-01-12 14:01:32 -0500

    first commit

---
+++ zsh/profile.zsh
@@ -0,0 +1,35 @@
+
+
+HISTFILE=~/.config/zsh/history
+HISTSIZE=10000
+SAVEHIST=5000
+PATH=$PATH:~/.cargo/bin:~/Scripts
+
+alias hx=helix
+alias ls=lsd
+alias ll='lsd --long --header --git'
+alias cat=bat
+alias tree='ls --tree'
+
+export EDITOR='helix'
+export GIT_EDITOR=$EDITOR
+export GIT_TOKEN='1234567890yuio'
+export WM='sway'
+
+function osc7-pwd() {
+    emulate -L zsh # also sets localoptions for us
+    setopt extendedglob
+    local LC_ALL=C
+    printf '\e]7;file://%s%s\e\' $HOST ${PWD//(#m)([^@-Za-z&-;_~])/%${(l:2::0:)$(([##16]#MATCH))}}
+}
+
+function chpwd-osc7-pwd() {
+    (( ZSH_SUBSHELL )) || osc7-pwd
+}
+
+function xplr() {
+    cd $(/bin/xplr $@ )
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook -Uz chpwd chpwd-osc7-pwd
